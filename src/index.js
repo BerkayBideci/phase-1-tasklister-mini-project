19df35d29aed4d3a50386441599e6b8bf9ace7d3
Berkay Bideci <berkaybideci@gmail.com>
2023-04-16 23:08:49 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('#create-task-form')
+  let list = document.querySelector("#tasks")
+  let text = document.querySelector('#new-task-description')
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    ToDoAdd(text.value)
+    form.reset()
+  })
+
+  function ToDoAdd(todo) {
+    let li = document.createElement("li")
+    let btn = document.createElement("button")
+    btn.addEventListener('click', deleteToDo)
+    li.textContent = `${todo} `
+    btn.textContent = "X"
+    li.appendChild(btn)
+    list.appendChild(li)
+  }
+
+  function deleteToDo(e) {
+    e.target.parentNode.remove()
+  }
 });
